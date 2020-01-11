903eeae65743e2b0d78e1a9fd67630d5b0e34565
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-01-11 17:53:45 +0100

    radjeno na korisnik get metodi

--- src/controller/korisnik/KorisnikServlet.java
+++ src/controller/korisnik/KorisnikServlet.java
@@ -1,6 +1,10 @@
 package controller.korisnik;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Map;
 
@@ -9,9 +13,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.util.DateTool;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dao.DaoInterface;
+import model.Identifiable;
+import model.Korisnik;
 
 /**
  * Servlet implementation class KorisnikServlet
@@ -38,11 +46,29 @@ public class KorisnikServlet extends HttpServlet {
 			Map<String, String[]> paramMap = request.getParameterMap();
 			if (paramMap.keySet().isEmpty()) {
 				response.getWriter().write(om.writeValueAsString(DaoInterface.korisnikDao.getAll()));
-				return;
 			} else if (paramMap.containsKey("id")) {
 				response.getWriter().write(om.writeValueAsString(DaoInterface.korisnikDao.get(Integer.parseInt(paramMap.get("id")[0]))));
-				return;
-			}
+			} else if(paramMap.containsKey("searchString")) {
+				final String searchString = paramMap.get("searchString")[0].trim();
+				ArrayList<Identifiable> list = DaoInterface.korisnikDao.get(k->{
+					if(!searchString.isEmpty()) {
+						Korisnik korisnik = (Korisnik)k;
+						String korisnikString = korisnik.getUsername() + " " + (new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(korisnik.getDatumRegistracije()) + " " + korisnik.getUloga().toString();
+						String[] searchArray = searchString.split(" ");
+						return Arrays.stream(searchArray).reduce(true, (value, element)-> value && korisnikString.contains(element), (value1, value2)-> value1 && value2);
+					}
+					else return true;
+				});
+				response.getWriter().write(om.writeValueAsString(list));
+				
+				}
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().close();
+			return;
+			
+			
+				
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			request.setAttribute("error", HttpServletResponse.SC_NOT_ACCEPTABLE);
