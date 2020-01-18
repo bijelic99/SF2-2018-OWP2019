14836d4e1521d093b0407d56a2e12ffcf0eff470
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-01-18 13:41:09 +0100

    implementirane klase SalaDao i TipProjekcijeDao

--- src/dao/SalaDao.java
+++ src/dao/SalaDao.java
@@ -1,9 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import model.Identifiable;
+import model.Sala;
+import model.Zanr;
 
 public class SalaDao implements DaoInterface {
 
@@ -27,20 +33,98 @@ public class SalaDao implements DaoInterface {
 
 	@Override
 	public Identifiable get(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Sala sala = null;
+		Connection connection = ConnectionManager.getConnection();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "";
+		try {
+			query = "select id, naziv from sala where id = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				sala = new Sala();
+				sala.setId(resultSet.getInt(1));
+				sala.setNaziv(resultSet.getString(2));
+				sala.setPodrzaniTipoviProjekcija(DaoInterface.tipProjekcijeDao.getTipProjekcijeForSala(sala.getId(), connection));
+				
+				
+			}
+
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+		
+		return sala;
 	}
 
 	@Override
 	public ArrayList<Identifiable> get(Predicate<Identifiable> filterFunction) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return getAll().stream().filter(filterFunction).collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	@Override
 	public ArrayList<Identifiable> getAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Identifiable> sale = new ArrayList<Identifiable>();
+		Connection connection = ConnectionManager.getConnection();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "";
+		try {
+			query = "select id, naziv from sala";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				Sala sala = new Sala();
+				sala.setId(resultSet.getInt(1));
+				sala.setNaziv(resultSet.getString(2));
+				sala.setPodrzaniTipoviProjekcija(DaoInterface.tipProjekcijeDao.getTipProjekcijeForSala(sala.getId(), connection));
+				
+				sale.add(sala);
+			}
+
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+		return sale;
 	}
 
 }
--- src/dao/TipProjekcijeDao.java
+++ src/dao/TipProjekcijeDao.java
@@ -1,9 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import model.Identifiable;
+import model.Sala;
+import model.TipProjekcije;
 
 public class TipProjekcijeDao implements DaoInterface {
 
@@ -27,20 +33,129 @@ public class TipProjekcijeDao implements DaoInterface {
 
 	@Override
 	public Identifiable get(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		TipProjekcije tipProjekcije = null;
+		Connection connection = ConnectionManager.getConnection();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "";
+		try {
+			query = "select id, naziv from tip_projekcije where id = ?";
+			preparedStatement.setInt(1, id);
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				tipProjekcije = new TipProjekcije();
+				tipProjekcije.setId(resultSet.getInt(1));
+				tipProjekcije.setNaziv(resultSet.getString(2));
+				
+				
+			}
+
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+		return tipProjekcije;
 	}
 
 	@Override
 	public ArrayList<Identifiable> get(Predicate<Identifiable> filterFunction) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return getAll().stream().filter(filterFunction).collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	@Override
 	public ArrayList<Identifiable> getAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ArrayList<Identifiable> tipoviProjekcije = new ArrayList<Identifiable>();
+		Connection connection = ConnectionManager.getConnection();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "";
+		try {
+			query = "select id, naziv from tip_projekcije";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				TipProjekcije tipProjekcije = new TipProjekcije();
+				tipProjekcije.setId(resultSet.getInt(1));
+				tipProjekcije.setNaziv(resultSet.getString(2));
+				tipoviProjekcije.add(tipProjekcije);
+			}
 
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+		return tipoviProjekcije;
+	}
+	public ArrayList<TipProjekcije> getTipProjekcijeForSala(int salaId, Connection connection) throws Exception {
+		ArrayList<TipProjekcije> tipoviProjekcije = new ArrayList<TipProjekcije>();
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "";
+		try {
+			query = "select id, naziv from tip_projekcije join tip_projekcije_u_sali on tip_projekcije.id = tip_projekcije_u_sali.tip_projekcije_id where sala_id = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, salaId);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				TipProjekcije tipProjekcije = new TipProjekcije();
+				tipProjekcije.setId(resultSet.getInt(1));
+				tipProjekcije.setNaziv(resultSet.getString(2));
+				tipoviProjekcije.add(tipProjekcije);
+			}
+
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+
+			}
+			
+
+		}
+		return tipoviProjekcije;
+	}
 }
